458ebc312a5384f7e7e88a85a09b74aff033baea
Enriquer <rb.micelios@gmail.com>
2026-09-12 00:31:52 -0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,2 +1,2 @@
-window.SUPABASE_URL = "https://iszsdvdtkfdvoagpyvfl.supabase.co";
+window.SUPABASE_URL = "https://iszsdvdtkfdvoagpyvfl.supabase.co/rest/v1/";
 window.SUPABASE_KEY = "sb_publishable_0NBdN51ASUas1NvYZohuXQ_zCbNCPJL";
